f95298c7d60cd302ad150a898353ef045cab3fa1
Jack Sloan <jbsloan10@gmail.com>
2021-09-02 16:04:27 -0500

    add shell component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { PathLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './components/shell/shell.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    pathMatch: 'full',
+    children: [],
+  },
+  // { path: '**', component:  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/shell/shell.component.ts
+++ src/app/components/shell/shell.component.ts
@@ -3,19 +3,30 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-shell',
   template: `
-    <p>
-      shell works!
-    </p>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a
+              *ngFor="let link of links"
+              routerLink="{{ link[0] }}"
+              routerLinkActive="text-blue-500"
+              [routerLinkActiveOptions]="{ exact: true }"
+            >
+              {{ link[1] }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <section>
+      <router-outlet></router-outlet>
+    </section>
   `,
-  styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShellComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ShellComponent {
+  links: [string, string][] = [['/', 'Home']];
 }
