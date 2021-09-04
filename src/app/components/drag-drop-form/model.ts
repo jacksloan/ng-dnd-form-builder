5950cbb4f88e9e8974a89e22e50418e50f3429aa
Jack Sloan <jbsloan10@gmail.com>
2021-09-04 17:42:07 -0500

    use fewer initial radio options

--- src/app/components/drag-drop-form/model.ts
+++ src/app/components/drag-drop-form/model.ts
@@ -58,11 +58,7 @@ export const dndFormInputRadio: DnDFormConfig = {
   templateOptions: {
     label: 'Radio Label',
     required: true,
-    options: [
-      { label: 'Option 1', value: 1 },
-      { label: 'Option 2', value: 2 },
-      { label: 'Option 3', value: 3 },
-    ],
+    options: [{ label: 'Option 1...', value: 1 }],
   },
 };
 
