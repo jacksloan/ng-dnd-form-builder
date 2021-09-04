f702d847756966cc90dd9c0862db29350f0c1778
Jack Sloan <jbsloan10@gmail.com>
2021-09-04 17:40:50 -0500

    set default options for radio input

--- src/app/components/drag-drop-form/model.ts
+++ src/app/components/drag-drop-form/model.ts
@@ -58,6 +58,11 @@ export const dndFormInputRadio: DnDFormConfig = {
   templateOptions: {
     label: 'Radio Label',
     required: true,
+    options: [
+      { label: 'Option 1', value: 1 },
+      { label: 'Option 2', value: 2 },
+      { label: 'Option 3', value: 3 },
+    ],
   },
 };
 
